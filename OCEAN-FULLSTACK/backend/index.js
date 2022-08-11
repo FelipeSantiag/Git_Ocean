8d1dbd5698414cf4690afbd19e353a1183e99ae8
Santi <felipesantiago.job@gmail.com>
2022-08-11 14:56:06 +0000

    Instalando Dev Nodemon do package.json

--- OCEAN-FULLSTACK/backend/index.js
+++ OCEAN-FULLSTACK/backend/index.js
@@ -6,11 +6,45 @@ app.get('/', function (req, res) {
 })
 
 app.get('/eu', function (req, res) {
-  res.send('Oi, meu nome eh Santi e estou nesta pagina')
+  res.send('Oilá, meu nome eh Santi e estou nesta pagina')
 })
 
 app.get('/oi', function (req, res) {
   res.send('Olá mundo')
 })
+
+//Isso é um comentario
+//signiica que esse cara não será lido.
+//first:Criar a lista com as pontuações
+//Obackend armazena as pontuações
+
+const lista = [
+  {
+    id: 1,
+    nome: "Paulo",
+    pontos: 21,
+  },
+  {
+    id: 2,
+    nome: "Daniel",
+    pontos: 52,
+  },
+  {
+    id: 3,
+    nome: "Beatriz",
+    pontos: 97,
+  },
+];
+//Endpoint READ ALL - [GET] /pontuações
+app.get("/pontuacoes", function(req, res){
+  res.send(lista);
+}
+);
+
+//Endpoint CREATE - [POST] / pontuações
+app.post("/pontuacoes", function(req,res){
+  res.send("Criar uma pontuação");
+}
+);
 
 app.listen(3000)
